de0de1b257a5738250bca421d0a52a1629a96676
dmitrenckoilya <dmitrencko.ilya2018@yandex.ru>
2017-03-15 21:13:02 -0400

    remove polymer firebase

--- Polymer/Firebase/polymerfire-test/sw-import.js
+++
@@ -1,5 +0,0 @@
-XSym
-0047
-729677fedefd807af99af5d392372cde
-bower_components/note-app-elements/sw-import.js
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
